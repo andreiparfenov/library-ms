1c6155dce7063a37ab209deba723e4ef9bdbd76e
Andrey <andreymail98@gmail.com>
2020-03-31 09:02:14 +0300

    add client side

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <BookComponent />
+  </div>
+</template>
+
+<script>
+import BookComponent from './components/BookComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BookComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/BookService.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+const url = 'http://localhost:5000/api/books';
+
